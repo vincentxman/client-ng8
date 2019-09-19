fe2d89dd444cdbd66e73171fc16e60cbf203d607
vincent <vincentxman@hotmail.com>
2019-09-19 14:22:57 +0800

    修改cs: ngbd -> app

--- src/app/shared/page-header/page-header.component.ts
+++ src/app/shared/page-header/page-header.component.ts
@@ -2,8 +2,7 @@ import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
-  templateUrl: './page-header.component.html',
-  styleUrls: ['./page-header.component.css']
+  templateUrl: './page-header.component.html'
 })
 export class PageHeaderComponent{
   @Input() title: string;
--- src/app/src/app/shared/page-wrapper/page-header.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PageHeader } from './page-header';
-
-describe('PageHeader', () => {
-  it('should create an instance', () => {
-    expect(new PageHeader()).toBeTruthy();
-  });
-});
