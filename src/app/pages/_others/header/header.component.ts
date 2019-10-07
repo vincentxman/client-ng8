8e2ff25dd951b6e2ef1ff3949279101900818a71
vincent <vincentxman@hotmail.com>
2019-10-07 22:06:23 +0800

    b

--- src/app/pages/_others/header/header.component.ts
+++ src/app/pages/_others/header/header.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+  navbarCollapsed = true;
   constructor() { }
   ngOnInit() {
   }
