d82c4aa99e81c9c115418bc8f86152a235d72903
vincent <vincentxman@hotmail.com>
2019-10-29 21:18:38 +0800

    tslint

--- src/app/02_router/_exercise/gh-cat/gh-cat.component.ts
+++ src/app/02_router/_exercise/gh-cat/gh-cat.component.ts
@@ -3,36 +3,39 @@ import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
 import { Subscription } from 'rxjs';
 
+const catsGql = gql`
+{
+  cats(limit: 10){
+    id,
+    name,
+    age
+  }
+}
+`;
+
+
 @Component({
   selector: 'app-gh-cat',
   templateUrl: './gh-cat.component.html',
   styleUrls: ['./gh-cat.component.css']
 })
 export class GhCatComponent implements OnInit, OnDestroy {
-  private cats: any[];
   private catsSubscription: Subscription;
-  private loading = true;
-  private error: any;
+  cats: any;
+  loading = true;
+  error: any;
+
   constructor(private apollo: Apollo) { }
 
   ngOnInit() {
     this.catsSubscription = this.apollo
       .watchQuery({
-        query: gql`
-        {
-          cats(limit: 10){
-            id,
-            name,
-            age
-          }
-        }
-        `,
+        query: catsGql,
       })
       .valueChanges.subscribe(result => {
         this.cats = result.data && result.data['cats'];
         this.loading = result.loading;
         this.error = result.errors;
-        console.log(this.cats);
       });
   }
   ngOnDestroy() {
