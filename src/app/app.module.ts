9238a924ce4164e18069d8fa2c48026797626fce
vincent <vincentxman@hotmail.com>
2019-11-06 23:17:18 +0800

    markdown 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
@@ -52,6 +53,7 @@ registerLocaleData(zh);
     FormsModule,
 
     BrowserAnimationsModule,
+    MarkdownModule.forRoot(),
 
     // TODO InMemoryDataService
     environment.isMemoDB ? HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }) : [],
