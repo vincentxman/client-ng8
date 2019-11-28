96674bc6b80a492b6353b7c125f317626ed9fe7a
vincent <vincentxman@hotmail.com>
2019-11-28 18:07:23 +0800

    login-2

---
+++ src/app/02_router/_backstage/login-2/_services/user.service.ts
@@ -0,0 +1,30 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../_models';
+
+@Injectable()
+export class UserService {
+  apiUrl = 'api';
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
+  }
+
+  getById(id: number) {
+    return this.http.get(`${this.apiUrl}/users/` + id);
+  }
+
+  register(user: User) {
+    return this.http.post(`${this.apiUrl}/users/register`, user);
+  }
+
+  update(user: User) {
+    return this.http.put(`${this.apiUrl}/users/` + user.id, user);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${this.apiUrl}/users/` + id);
+  }
+}
