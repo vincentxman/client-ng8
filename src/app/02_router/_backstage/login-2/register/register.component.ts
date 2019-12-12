a665a00f80e4c685ea8cb6cca3e358466ef55c2d
vincent <vincentxman@hotmail.com>
2019-12-12 12:53:46 +0800

    b

--- src/app/02_router/_backstage/login-2/register/register.component.ts
+++ src/app/02_router/_backstage/login-2/register/register.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class Register3Component implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this.initForm();
   }
 
+  initForm() {
+    this.form = this._formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      firstName: '',
+      lastName: '',
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.displayValidationErrors();
+      return;
+    }
+
+    // const registerVm: RegisterVm = new RegisterVm(this.form.value);
+    // this._userClient.register(registerVm)
+    //   .pipe(catchError((err: ApiException) => throwError(err)))
+    //   .subscribe((user: UserVm) => {
+    //     dump(user, '_userClient.register');
+    //     this._router.navigate(['/backstage/login']);
+    //   }, (err: ApiException) => {
+    //     dumpErr(err, '_userClient.register');
+    //   });
+  }
+
+  private displayValidationErrors() {
+    const formKeys = Object.keys(this.form.controls);
+    formKeys.forEach(key => {
+      this.form.controls[key].markAsDirty();
+      this.form.controls[key].updateValueAndValidity();
+    });
+  }
 }
